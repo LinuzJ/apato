14670552dd444d244852a6a7c2a4a3e1ae397b68
LinuzJ <linusjern00@gmail.com>
2023-06-28 23:37:30 +0300

    Add working token fetching

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,14 @@
 #[macro_use]
 extern crate rocket;
 
-use rand::Rng;
-use reqwest::Client;
-use reqwest::Response;
-use reqwest::header::{HeaderMap, HeaderValue};
+mod fetch_tokens;
 
-struct OikotieTokens {
-    loaded: String,
-    cuid: String,
-    token: String,
-}
-
-async fn fetch_oikotie_tokens() -> Result<(), reqwest::Error> {
-    let client = reqwest::Client::new();
-    let random_number: String = rand
-        ::thread_rng()
-        .gen_range(5000..10000)
-        .to_string();
-    let params: Vec<(&str, &str)> = vec![("format", "json"), ("rand", &random_number)];
-
-    let mut headers = HeaderMap::new();
-    headers.insert("user-agent", HeaderValue::from_static("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"));
-
-    let response = client
-        .get("https://asunnot.oikotie.fi/user/get")
-        .query(&params)
-        .headers(headers)
-        .send()
-        .await?;
-
-    // Process the response as needed
-    println!("Response status: {}", response.status());
-    let body = response.text().await?;
-    println!("Response body:\n{}", body);
-
-    Ok(())
-}
+use fetch_tokens::get_tokens;
 
 #[get("/")]
-async fn index() -> &'static str {
-    let tokens = fetch_oikotie_tokens().await;
-    "heh"
+async fn index() -> String {
+    let tokens: fetch_tokens::OikotieTokens = get_tokens().await;
+    tokens.token
 }
 
 #[rocket::main]
--- test.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-x = requests.get('https://asunnot.oikotie.fi/user/get?format=json&rand=7123')
-print(x.json())
